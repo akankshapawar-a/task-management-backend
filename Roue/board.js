928ef35923a087769b3b9d70a090dae15ab4ad39
Akanksha Pawar <apawar@intraedge.com>
2025-09-28 23:13:01 +0530

    description

--- Roue/board.js
+++ Roue/board.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addCard, createColumn, getColumn,getCardData, updateCardLables, updateCardDates,deleteCardsDate} from '../controller/Column.js';
+import { addCard, createColumn, getColumn,getCardData, updateCardLables, updateCardDates,deleteCardsDate,updatedDescription} from '../controller/Column.js';
 import authMiddleware from '../middleware/authentication.js';
 
 const boardRouter=express.Router();
@@ -11,4 +11,5 @@ boardRouter.put('/addcards/label/:cardId',authMiddleware,updateCardLables);
 boardRouter.get('/cards/:id',authMiddleware,getCardData);
 boardRouter.put('/cards/dates/:cardId',authMiddleware,updateCardDates);
 boardRouter.delete('/cards/delete/date/:cardId',authMiddleware,deleteCardsDate);
+boardRouter.put('/card/description/:cardId',authMiddleware,updatedDescription);
 export default boardRouter;
